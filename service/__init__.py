ff929b322d9dfaacce3d03aad36d336ead34ef61
lojoja <dev@lojoja.com>
2024-10-23 11:27:52 -0400

    Re-export the public API

--- service/__init__.py
+++ service/__init__.py
@@ -4,6 +4,6 @@ service
 The service public API
 """
 
-from . import launchctl
-from .service import locate
-from .service import Service
+from . import launchctl as launchctl
+from .service import locate as locate
+from .service import Service as Service
